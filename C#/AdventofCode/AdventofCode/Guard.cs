cbdf8a8e44b70f8d2cabd8a7cbfc554f4a7ea550
Daan Stout <formaestroke@gmail.com>
2018-12-08 13:53:24 +0100

    Initial Push

---
+++ C#/AdventofCode/AdventofCode/Guard.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventofCode {
+    public class GuardDataTable {
+        public List<GuardData> data { get; private set; }
+
+        public GuardDataTable() {
+            data = new List<GuardData>();
+        }
+
+        public void AddData(GuardData guardData) {
+            if (data.Count == 0) {
+                data.Add(guardData);
+                return;
+            }
+
+            for (int i = 0; i < data.Count; i++) {
+                if (DateTime.Compare(guardData.dateTime, data[i].dateTime) < 0) {
+                    data.Insert(i, guardData);
+                    return;
+                }
+            }
+
+            data.Add(guardData);
+        }
+    }
+
+    public class GuardData {
+        public DateTime dateTime { get; private set; }
+        public string text { get; private set; }
+
+        public GuardData(string data) {
+            int year = Convert.ToInt32(data.Substring(1, 4));
+            int month = Convert.ToInt32(data.Substring(6, 2));
+            int day = Convert.ToInt32(data.Substring(9, 2));
+            int hour = Convert.ToInt32(data.Substring(12, 2));
+            int minute = Convert.ToInt32(data.Substring(15, 2));
+            dateTime = new DateTime(year, month, day, hour, minute, 0);
+            text = data.Substring(19);
+        }
+
+        public override string ToString() {
+            return $"dateTime: {dateTime}; Text: {text}";
+        }
+    }
+
+    public class GuardSchedule {
+        public DateTime dateTime { get; private set; }
+        public int guardId { get; private set; }
+        public bool[] isAwake { get; private set; }
+
+        public GuardSchedule(int id, DateTime dt) {
+            guardId = id;
+            dateTime = dt;
+
+            isAwake = new bool[60];
+
+            for (int i = 0; i < 60; i++)
+                isAwake[i] = true;
+        }
+
+        public void SetAsleep(int start, int end) {
+            for (int i = start; i < end; i++)
+                isAwake[i] = false;
+        }
+
+        public override string ToString() {
+            string returnString = $"{dateTime.Month}-{dateTime.Day}\t#{guardId}\t";
+
+            for (int i = 0; i < 60; i++)
+                returnString += isAwake[i] ? '.' : '#';
+
+            return returnString;
+        }
+    }
+
+    public class GuardInfo {
+        public int guardId { get; private set; }
+        public int[] minutesAsleep { get; private set; }
+
+        public GuardInfo(int id) {
+            guardId = id;
+
+            minutesAsleep = new int[60];
+
+            for (int i = 0; i < 60; i++)
+                minutesAsleep[i] = 0;
+        }
+
+        public void AddInfo(GuardSchedule schedule) {
+            for (int i = 0; i < 60; i++)
+                minutesAsleep[i] += schedule.isAwake[i] ? 0 : 1;
+        }
+
+        public int TimeSlept() {
+            int total = 0;
+
+            for (int i = 0; i < 60; i++)
+                total += minutesAsleep[i];
+
+            return total;
+        }
+
+        public int MinuteAsleep() {
+            int mostAsleep = 0;
+
+            for (int i = 1; i < 60; i++)
+                if (minutesAsleep[mostAsleep] < minutesAsleep[i])
+                    mostAsleep = i;
+
+            return mostAsleep;
+        }
+
+        public int MostAsleep() {
+            int i = 0;
+
+            for (int j = 1; j < 60; j++)
+                if (minutesAsleep[i] < minutesAsleep[j])
+                    i = j;
+
+            return i;
+        }
+
+        public override string ToString() {
+            string returnString = $"ID: {guardId} - ";
+
+            for (int i = 0; i < 60; i++) {
+                returnString += $"{minutesAsleep[i]} ";
+            }
+
+            return returnString;
+        }
+    }
+}
---
+++ C#/AdventofCode/AdventofCode/Tree.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventofCode {
+    public class Tree {
+        public Node root;
+
+        private Node current;
+
+        public void ProcessInput(int[] input) {
+            CreateNode(input, null, 0);
+        }
+
+        private int[] CreateNode(int[] input, Node parent, int childIndex) {
+            if (input.Length == 0)
+                return input;
+
+
+            Node n = new Node(input[0], input[1]);
+            if (parent != null)
+                parent.childNodes[childIndex] = n;
+            else
+                root = n;
+
+            {
+                int[] temp = new int[input.Length - 2];
+                Array.Copy(input, 2, temp, 0, input.Length - 2);
+                input = temp;
+            }
+
+            if (n.childNodes.Length > 0) {
+                for (int i = 0; i < n.childNodes.Length; i++) {
+                    input = CreateNode(input, n, i);
+                }
+            }
+
+            if (input.Length == 0)
+                return input;
+
+
+            if (n.metaData.Length > 0) {
+                if (n.metaData.Length <= input.Length) {
+                    for (int i = 0; i < n.metaData.Length; i++) {
+                        n.metaData[i] = input[i];
+                    }
+                    int[] temp = new int[input.Length - n.metaData.Length];
+                    Array.Copy(input, n.metaData.Length, temp, 0, temp.Length);
+                    input = temp;
+                }
+            }
+
+            return input;
+        }
+
+        public int GetMeta() {
+            if (root == null)
+                return 0;
+
+            return GetMeta(root);
+        }
+
+        private int GetMeta(Node n) {
+            if (n == null)
+                return 0;
+
+            int meta = 0;
+
+            foreach (Node child in n.childNodes)
+                meta += GetMeta(child);
+
+            foreach (int i in n.metaData)
+                meta += i;
+
+            return meta;
+        }
+
+        public int GetRootMeta() {
+            if (root == null)
+                return 0;
+
+            return GetRootMeta(root);
+        }
+
+        private int GetRootMeta(Node n) {
+            if (n == null)
+                return 0;
+
+            int meta = 0;
+
+            if (n.childNodes.Length == 0) {
+                meta += GetMeta(n);
+            } else {
+                foreach (int i in n.metaData) {
+                    if (i == 0 || i > n.childNodes.Length)
+                        continue;
+
+                    meta += GetRootMeta(n.childNodes[i - 1]);
+                }
+            }
+
+            return meta;
+        }
+    }
+
+
+    public class Node {
+        public Node[] childNodes;
+        public int[] metaData;
+
+        public Node(int Nodes, int metaDatas) {
+            childNodes = new Node[Nodes];
+            metaData = new int[metaDatas];
+        }
+    }
+}
