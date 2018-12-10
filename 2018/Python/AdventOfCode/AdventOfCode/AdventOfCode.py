da6fb74c1a201d46d6f46fb3bf87f5ab3d3b0169
Daan Stout <formaestroke@gmail.com>
2018-12-10 20:06:09 +0100

    added day 2 question 1 for python

--- 2018/Python/AdventOfCode/AdventOfCode/AdventOfCode.py
+++ 2018/Python/AdventOfCode/AdventOfCode/AdventOfCode.py
@@ -5,7 +5,7 @@ def Day1_Question1():
 
 	file = open(path + "Advent of Code Calibration values.txt", "r")
 	input = ""
-	if(file.mode == 'r'):
+	if file.mode == 'r':
 		input = file.read()
 	else:
 		return
@@ -16,20 +16,19 @@ def Day1_Question1():
 
 	for change in inputs:
 		action = change[0]
-		if(action == '+'):
+		if action == '+':
 			frequency += int(change[1:])
-		elif(action == '-'):
+		elif action == '-':
 			frequency -= int(change[1:])
 
 	print("Answer Q1.1: " + str(frequency) + '\n')
-
 
 def Day1_Question2():
 	print("Day 1 - Question 2")
 
 	file = open(path + "Advent of Code Calibration values.txt", "r")
 	input = ""
-	if(file.mode == 'r'):
+	if file.mode == 'r':
 		input = file.read()
 	else:
 		return
@@ -40,27 +39,65 @@ def Day1_Question2():
 	usedFrequencies = []
 	doubleFound = False
 
-	while(not doubleFound):
+	while not doubleFound:
 		for change in inputs:
 			action = change[0]
-			if(action == '+'):
+			if action == '+':
 				frequency += int(change[1:])
-			elif(action == '-'):
+			elif action == '-':
 				frequency -= int(change[1:])
 
-			if(frequency in usedFrequencies):
-				doubleFound = True;
-				break;
+			if frequency in usedFrequencies:
+				doubleFound = True
+				break
 			else:
 				usedFrequencies.append(frequency)
 
 	print("Answer Q1.2: " + str(frequency) + '\n')
 
+def Day2_Question1():
+    print("Day 2 - Question 1")
+
+    file = open(path + "Advent of Code Box Ids.txt", "r")
+    input = ""
+    if file.mode == 'r':
+        input = file.read()
+    else:
+        return
+    
+    inputs = input.split('\n')
+
+    twice = 0
+    thrice = 0
+
+    for id in inputs:
+        existOnce = []
+        existTwice = []
+        existThrice = []
+
+        for char in id:
+            if char in existThrice:
+                existThrice.remove(char)
+            elif char in existTwice:
+                existTwice.remove(char)
+                existThrice.append(char)
+            elif char in existOnce:
+                existOnce.remove(char)
+                existTwice.append(char)
+            else:
+                existOnce.append(char)
+
+        if len(existThrice) >= 1:
+            thrice += 1
+
+        if len(existTwice) >= 1:
+            twice += 1
+
+    print("Answer Q2.1: " + str(twice * thrice))
+
 
 if(__name__ == "__main__"):
-	print("Advent of Code - Year 2018")
-	Day1_Question1()
-	Day1_Question2()
-
-
-
+    print("Advent of Code - Year 2018")
+    Day2_Question1()
+	#Day1_Question1()
+	#Day1_Question2()
