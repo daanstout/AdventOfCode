7fc9aee0dea4ea6638f04278db38bc9dfd31ed63
maestroke <formaestroke@gmail.com>
2018-12-11 17:28:44 +0100

    Day 3 and 4 for year 2015

--- 2015/C#/AdventOfCode/AdventOfCode/Program.cs
+++ 2015/C#/AdventOfCode/AdventOfCode/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AdventOfCode {
     class Program {
@@ -12,8 +15,10 @@ namespace AdventOfCode {
 
             //Day1_Question1();
             //Day1_Question2();
-            Day2_Question1();
-            Day2_Question2();
+            //Day2_Question1();
+            //Day2_Question2();
+            //Day3();
+            Day4();
 
             Console.WriteLine("Done! Press a key to end the console.");
 
@@ -23,7 +28,7 @@ namespace AdventOfCode {
         private static void Day1_Question1() {
             Console.WriteLine("Day 1 - Question 1:");
 
-            string input = File.ReadAllText($"{path}Advent of Code Floors.txt");
+            string input = File.ReadAllText($"{path}Advent of Code - Day 1 - Floors.txt");
 
             int floor = 0;
 
@@ -36,14 +41,14 @@ namespace AdventOfCode {
         private static void Day1_Question2() {
             Console.WriteLine("Day 1 - Question 2:");
 
-            string input = File.ReadAllText($"{path}Advent of Code Floors.txt");
+            string input = File.ReadAllText($"{path}Advent of Code - Day 1 - Floors.txt");
 
             int floor = 0;
-            
-            for(int i = 0; i < input.Length; i++) {
+
+            for (int i = 0; i < input.Length; i++) {
                 floor += input[i] == '(' ? 1 : -1;
 
-                if(floor == -1) {
+                if (floor == -1) {
                     Console.WriteLine($"Answer Q1.2: {i + 1}\n");
                     break;
                 }
@@ -61,7 +66,7 @@ namespace AdventOfCode {
 
             int totalArea = 0;
 
-            foreach(string s in inputs) {
+            foreach (string s in inputs) {
                 int l, w, h;
                 string[] sizes = s.Trim().Split('x');
                 l = Convert.ToInt32(sizes[0]);
@@ -108,6 +113,144 @@ namespace AdventOfCode {
             }
 
             Console.WriteLine($"Answer Q2.2: {ribbonLength}\n");
+        }
+
+        private static void Day3() {
+            string input = File.ReadAllText($"{path}Advent of Code - Day 3 - Directions.txt");
+
+            {
+                Console.WriteLine("Day 3 - Question 1:");
+
+                Dictionary<Point, int> map = new Dictionary<Point, int>();
+
+                int x = 0, y = 0;
+                map.Add(new Point(x, y), 1);
+
+                foreach (char c in input) {
+                    switch (c) {
+                        case '^':
+                            y += 1;
+                            break;
+                        case '>':
+                            x += 1;
+                            break;
+                        case 'v':
+                            y -= 1;
+                            break;
+                        case '<':
+                            x -= 1;
+                            break;
+                    }
+
+                    if (map.ContainsKey(new Point(x, y)))
+                        map[new Point(x, y)] += 1;
+                    else
+                        map.Add(new Point(x, y), 1);
+                }
+
+                Console.WriteLine($"Answer Q3.1: {map.Values.Count}\n");
+            }
+            {
+                Console.WriteLine("Day 3 - Question 2:");
+
+                Dictionary<Point, int> map = new Dictionary<Point, int>();
+
+                int santaX = 0, santaY = 0;
+                int robotX = 0, robotY = 0;
+
+                map.Add(new Point(santaX, santaY), 1);
+
+                bool santa = true;
+
+                foreach (char c in input) {
+                    switch (c) {
+                        case '^':
+                            if (santa)
+                                santaY += 1;
+                            else
+                                robotY += 1;
+                            break;
+                        case '>':
+                            if (santa)
+                                santaX += 1;
+                            else
+                                robotX += 1;
+                            break;
+                        case 'v':
+                            if (santa)
+                                santaY -= 1;
+                            else
+                                robotY -= 1;
+                            break;
+                        case '<':
+                            if (santa)
+                                santaX -= 1;
+                            else
+                                robotX -= 1;
+                            break;
+                    }
+
+                    Point house;
+
+                    if (santa)
+                        house = new Point(santaX, santaY);
+                    else
+                        house = new Point(robotX, robotY);
+
+                    if (map.ContainsKey(house))
+                        map[house] += 1;
+                    else
+                        map.Add(house, 1);
+
+                    santa = !santa;
+                }
+
+                Console.WriteLine($"Answer Q3.2: {map.Values.Count}\n");
+            }
+        }
+
+        private static void Day4() {
+            string input = "yzbqklnj";
+
+            using (MD5 hash = MD5.Create()) {
+                Console.WriteLine("Day 4 - Question 1:");
+
+                int i = 1;
+                while (true) {
+                    string hashed = GetMD5Hash(hash, input + i);
+
+                    if(hashed.Substring(0, 5).Equals("00000")) {
+                        Console.WriteLine($"Answer Q4.1: {i}\n");
+                        break;
+                    }
+
+                    i++;
+                }
+            }
+
+            using (MD5 hash = MD5.Create()) {
+                Console.WriteLine("Day 4 - Question 2:");
+
+                int i = 1;
+                while (true) {
+                    string hashed = GetMD5Hash(hash, input + i);
+
+                    if (hashed.Substring(0, 6).Equals("000000")) {
+                        Console.WriteLine($"Answer Q4.2: {i}\n");
+                        break;
+                    }
+
+                    i++;
+                }
+            }
+        }
+
+        private static string GetMD5Hash(MD5 hash, string input) {
+            byte[] data = hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < data.Length; i++) 
+                sb.Append(data[i].ToString("x2"));
+            return sb.ToString();
         }
     }
 }
